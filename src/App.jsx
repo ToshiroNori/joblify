64645781d625921f0f7714f3682ea428878a473f
Daryl Sabado <sabado114daryl@gmail.com>
2025-05-03 16:01:01 +0800

    change path

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
       <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
   );
